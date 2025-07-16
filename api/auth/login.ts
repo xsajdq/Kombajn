dcbf3eb4a5ebe04cd0271a57e2f656756e94e4d4
xsajdq <39928511+xsajdq@users.noreply.github.com>
2025-07-16 10:34:42 +0200

    Add files via upload

---
+++ api/auth/login.ts
@@ -0,0 +1,4 @@
+// api/auth/login.ts
+import loginHandler from '../_handlers/auth/login';
+
+export default loginHandler;
---
+++ api/auth/signup.ts
@@ -0,0 +1,4 @@
+// api/auth/signup.ts
+import signupHandler from '../_handlers/auth/signup';
+
+export default signupHandler;
